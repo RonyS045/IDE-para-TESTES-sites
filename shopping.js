479dd66377da82743fe76ac47ebc9311f04c53f5
RonyS <rony.sap@hotmail.com>
2024-10-26 19:47:23 -0300

    ProjectContinue261026

--- shopping.js
+++ shopping.js
@@ -62,3 +62,19 @@ window.onload = () => {
         updateCart();
     }
 };
+
+
+let toggler = document.getElementById("switch");
+
+toggler.addEventListener("click", () => {
+  //   if (toggler.checked === true) {
+  //     document.body.style.backgroundColor = "black";
+  //   } else {
+  //     document.body.style.backgroundColor = "white";
+  //   }
+
+  toggler.checked === true
+    ? (document.body.style.backgroundColor = "black")
+    : (document.body.style.backgroundColor = "white");
+});
+
